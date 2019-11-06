118fabc4b5b14a16c25567c01ea96cd56d5620c7
15100213 <765696976@qq.com>
2019-11-06 18:59:36 +0800

    样式调整

--- src/views/bigData/commonReport.vue
+++ src/views/bigData/commonReport.vue
@@ -40,7 +40,7 @@
   <el-divider direction="vertical"></el-divider><span>查询条件配置</span>
 </div>
 <div>
-    <el-button type="primary" style="float:right;margin:50px 15px 5px 0;" plain>
+    <el-button type="primary" @click.native="dialogFormVisible=true" style="float:right;margin:50px 15px 5px 0;" plain>
         新增查询条件
     </el-button>
  </div>
@@ -69,7 +69,7 @@
   <el-divider direction="vertical"></el-divider><span>展示要素</span>
 </div>
 <div>
-    <el-button type="primary" style="float:right;margin:50px 15px 5px 0;" plain>
+    <el-button type="primary" @click.native="dialogFormVisible2=true"  style="float:right;margin:50px 15px 5px 0;" plain>
         新增展现要素
     </el-button>
  </div>
@@ -107,7 +107,66 @@
         <el-button type="primary"> 发 布 </el-button>
       </el-col>
 </el-row>
+<div class="mydialog">
+    <el-dialog :title="title" :visible.sync="dialogFormVisible" >
+            <el-form :model="form"  ref="form"  >
+                <el-form-item label="条件名称" :label-width="formLabelWidth"  >
+                  <el-input v-model="tjmc" autocomplete="off" placeholder="请输入条件名称"></el-input>
+                </el-form-item>
+                <el-form-item label="条件编码" :label-width="formLabelWidth"  >
+                  <el-input v-model="tjbm" autocomplete="off"  placeholder="请输入条件编码"></el-input>
+                </el-form-item>
+                <el-form-item label="条件类型" :label-width="formLabelWidth"  prop="sort">
+                  <el-select v-model="tjlx" placeholder="请选择条件类型">
+                      <el-option label="文本" value="文本"></el-option>
+                      <el-option label="值范围空间" value="值范围空间"></el-option>
+                    </el-select>
+                </el-form-item>
+                 <el-form-item label="是否必填" :label-width="formLabelWidth" prop="desc">
+                  <el-radio v-model="sfbt" label="是">是</el-radio>
+                  <el-radio v-model="sfbt" label="否">否</el-radio>
+                </el-form-item>
+                <el-form-item label="默认值" :label-width="formLabelWidth" prop="desc">
+                  <el-input  v-model="mrz"  autocomplete="off"  placeholder="请输入默认值"></el-input>
+                </el-form-item>
+                <el-form-item label="排序" :label-width="formLabelWidth" prop="desc">
+                  <el-input  v-model="px"   autocomplete="off"  placeholder="请输入排序"></el-input>
+                </el-form-item>
+                <el-form-item label="" :label-width="formLabelWidth" >
+                    <el-button type="warning" @click="dialogFormVisible = false">取 消</el-button>
+                    <el-button type="success" @click.native="addReq">保 存</el-button>
+                </el-form-item>
+            </el-form>
+
+        </el-dialog>
+        <el-dialog title="新增展示元素" :visible.sync="dialogFormVisible2" >
+            <el-form :model="form"  ref="form"  >
+                <el-form-item label="要素名称" :label-width="formLabelWidth"  >
+                  <el-input v-model="ysmc" autocomplete="off" placeholder="请输入要素名称"></el-input>
+                </el-form-item>
+                <el-form-item label="要素编码" :label-width="formLabelWidth"  >
+                  <el-input v-model="ysbm" autocomplete="off"  placeholder="请输入要素编码"></el-input>
+                </el-form-item>
+                 <el-form-item label="是否展示" :label-width="formLabelWidth" prop="desc">
+                  <el-radio v-model="sfzs" label="是">是</el-radio>
+                  <el-radio v-model="sfzs" label="否">否</el-radio>
+                </el-form-item>
+                <el-form-item label="SQL表达式" :label-width="formLabelWidth" prop="desc">
+                  <el-input  v-model="bds"  autocomplete="off"  placeholder="请输入SQL表达式"></el-input>
+                </el-form-item>
+                <el-form-item label="排序" :label-width="formLabelWidth" prop="desc">
+                  <el-input  v-model="px"   autocomplete="off"  placeholder="请输入排序"></el-input>
+                </el-form-item>
+                <el-form-item label="" :label-width="formLabelWidth" >
+                    <el-button type="warning" @click="dialogFormVisible2 = false">取 消</el-button>
+                    <el-button type="success" @click.native="addys">保 存</el-button>
+                </el-form-item>
+            </el-form>
+
+        </el-dialog>
+    </div>
 </div>
+
 </template>
 <script>
 export default {
@@ -118,13 +177,48 @@ export default {
     },
     deleteRow(index, rows) {
       rows.splice(index, 1);
+    },
+    addReq(){
+        var arr={};
+        arr.tjmc1=this.tjmc;
+        arr.tjbm1=this.tjbm;
+        arr.tjlx1=this.tjlx;
+        arr.sfbt1=this.sfbt;
+        arr.mrz1=this.mrz;
+        arr.px=this.px;
+        this.tableData1.push(arr);
+        this.dialogFormVisible = false;
+    },
+    addys(){
+        var arr={};
+        arr.ysmc2=this.ysmc;
+        arr.ysbm2=this.ysbm;
+        arr.sql2=this.bds;
+        arr.sfzs2=this.sfzs;
+        arr.px2=this.px;
+        this.tableData2.push(arr);
+        this.dialogFormVisible2 = false;
     }
   },
   data() {
     return {
+       formLabelWidth: '120px',
+      dialogFormVisible:false,
+      dialogFormVisible2:false,
+      title:'查询条件配置',
       bbmc: '',
       bburl: '',
       radio: '',
+      tjmc:'',
+      tjbm:'',
+      tjlx:'',
+      sfbt:'',
+      mrz:'',
+      px:'',
+      ysmc:'',
+      ysbm:'',
+      bds:'',
+      sfzs:'',
       tableData1: [{
         tjmc1: '项目名称',
         tjbm1: 'PROJECT_NAME',
